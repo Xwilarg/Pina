6711a1f9d2c898d366113299c6508cd4c2c21adb
Xwilarg <xwilarg@yahoo.fr>
2019-05-29 18:09:10 +0200

    Add empty Discord bot

---
+++ Pina/CommunicationModule.cs
@@ -0,0 +1,33 @@
+﻿using Discord;
+using Discord.Commands;
+using DiscordUtils;
+using System;
+using System.Threading.Tasks;
+
+namespace Pina
+{
+    class CommunicationModule : ModuleBase
+    {
+        [Command("Info")]
+        private async Task Info()
+        {
+            await ReplyAsync("", false, Utils.GetBotInfo(Program.P.StartTime, "Pina", Program.P.client.CurrentUser));
+        }
+
+        [Command("Help")]
+        private async Task Help()
+        {
+            await ReplyAsync("", false, new EmbedBuilder()
+            {
+                Color = Color.Purple,
+                Title = "Help",
+                Description =
+                    "I'm here to simplify the process of pinning messages." + Environment.NewLine +
+                    "Please make sure that I have the 'Manage Messages' permission (I need it to pin messages)" + Environment.NewLine +
+                    "Then you have 2 ways to pin a message:" + Environment.NewLine +
+                    "Add a 📌 reaction to the message you want to pin" + Environment.NewLine +
+                    "Do the 'Pin' command followed by the ID of the message you want to pin"
+            }.Build());
+        }
+    }
+}
---
+++ Pina/Program.cs
@@ -0,0 +1,58 @@
+﻿using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+using DiscordUtils;
+using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace Pina
+{
+    class Program
+    {
+        public static async Task Main()
+            => await new Program().MainAsync();
+
+        public readonly DiscordSocketClient client;
+        private readonly CommandService commands = new CommandService();
+
+        public DateTime StartTime { private set; get; }
+        public static Program P { private set; get; }
+
+        private Program()
+        {
+            P = this;
+            client = new DiscordSocketClient(new DiscordSocketConfig
+            {
+                LogLevel = LogSeverity.Verbose,
+            });
+            client.Log += Utils.Log;
+            commands.Log += Utils.LogError;
+        }
+
+        private async Task MainAsync()
+        {
+            client.MessageReceived += HandleCommandAsync;
+
+            await commands.AddModuleAsync<CommunicationModule>(null);
+
+            await client.LoginAsync(TokenType.Bot, File.ReadAllText("Keys/token.txt"));
+            StartTime = DateTime.Now;
+            await client.StartAsync();
+
+            await Task.Delay(-1);
+        }
+
+        private async Task HandleCommandAsync(SocketMessage arg)
+        {
+            SocketUserMessage msg = arg as SocketUserMessage;
+            if (msg == null || arg.Author.IsBot) return;
+            int pos = 0;
+            if (msg.HasMentionPrefix(client.CurrentUser, ref pos) || msg.HasStringPrefix("p.", ref pos))
+            {
+                SocketCommandContext context = new SocketCommandContext(client, msg);
+                await commands.ExecuteAsync(context, pos, null);
+            }
+        }
+    }
+}
